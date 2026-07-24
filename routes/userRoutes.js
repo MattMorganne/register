f296cae6081fc3f4141a6c3eac0075f79fed0ece
Matoka <mattmorgy241@gmail.com>
2026-07-24 19:35:01 +0100

    Mise a jour de User route
    J'ai ajouter le middeelware en commentaire

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+//const auth = require("../middleware/auth"); // Import the auth middleware
 
 // Define routes
-router.get("/", userController.getAllUsers);
-router.get("/:email", userController.getUserByEmail);
-router.post("/", userController.createUser);
+router.get("/",  userController.getAllUsers);
+router.get("/:email",   userController.getUserByEmail);
+//router.post("/",   audit, userController.createUser);
 
 module.exports = router;
